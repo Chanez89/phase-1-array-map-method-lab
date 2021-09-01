56f205ea835ca7d1dff27bb898dcae548eb5b6af
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-01 15:50:21 -0700

    commit tree 56e37504cf2091bfcd82a77d85dff574b965c313

--- index.js
+++ index.js
@@ -11,6 +11,46 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  })
 }
+
+// tutorials.map(tutorial => {
+//                                            //["what","does","the","this","keyword","mean?"]
+//    tutorial.split(' ').map(word => {
+//      return word.charAt(0).toUpperCase() + word.slice(1)
+//    });
+   //["what", "does", "the", "this", "keyword"].map{word = >}
+
+//elements.join() joins strings together in an array
+
+// const aolUsername = 'xXxCaNdIxXx';    EXAMPLE of capitalizing 1st letter and lowercasing the rest.
+// //Xxxcandixxx
+
+
+// console.log(aolUsername.charAt(0).toUpperCase() + aolUsername.slice(1).toLowerCase());
+
+//   return titleCased;
+// }
+
+
+
+
+//split method
+
+//const str = 'The quick brown fox jumps over the lazy dog.';
+
+// const words = str.split(' ');
+// console.log(words);
+
+
+
+//let stepOne = tutorials.map(tutorial => tutorial.split(' '))
+//let stepTwo = stepOne.map(tutorial => tutorial.map(word => word.charAt(0)))
+//let stepThree = stepTwo.map(tutorialArray => tutorialArray.join(' '))
